8547190276d08a6add67a073db20ba64f51f30c4
nrufino15 <nrufino@elpuig.xeill.net>
2019-02-04 17:15:37 +0100

    Clase Main creada

---
+++ src/com/company/MP05/UF2/NF4/A2/ex2/HashTable.java
@@ -0,0 +1,199 @@
+package com.company.MP05.UF2.NF4.A2.ex2;
+
+// Original source code: https://gist.github.com/amadamala/3cdd53cb5a6b1c1df540981ab0245479
+// Modified by Fernando Porrino Serrano for academic purposes.
+
+import java.util.ArrayList;
+
+public class HashTable {
+    private int INITIAL_SIZE = 16;
+    private int size = 0;
+    private HashEntry[] entries = new HashEntry[INITIAL_SIZE];
+
+    public int size(){
+        return this.size;
+    }
+
+    public int realSize(){
+        return this.INITIAL_SIZE;
+    }
+
+    public void put(String key, String value) {
+        int hash = getHash(key);
+        final HashEntry hashEntry = new HashEntry(key, value);
+
+        if(entries[hash] == null) {
+            entries[hash] = hashEntry;
+        }
+        else if (entries[hash].key == hashEntry.key){
+            entries[hash].value = hashEntry.value;
+        } else {
+            HashEntry temp = entries[hash];
+            while(temp.next != null)
+                temp = temp.next;
+
+            temp.next = hashEntry;
+            hashEntry.prev = temp;
+        }
+    }
+
+    /**
+     * Returns 'null' if the element is not found.
+     */
+    public String get(String key) {
+        int hash = getHash(key);
+        if(entries[hash] != null) {
+            HashEntry temp = entries[hash];
+
+            while( !temp.key.equals(key))
+                temp = temp.next;
+
+            return temp.value;
+        }
+
+        return null;
+    }
+
+    public void drop(String key) {
+        int hash = getHash(key);
+        if(entries[hash] != null) {
+
+            HashEntry temp = entries[hash];
+            while( !temp.key.equals(key))
+                temp = temp.next;
+            //El IF estaba mal. A la hora de borrar un solo elemento borraba toda la linea no el seleccionado.
+            //Codigo antiguo: if(temp.prev == null) entries[hash] = null;
+            //El codigo nuevo lo que nos dice es: Si temp.next es diferente de null y el temp.prev es nulo
+            //que la entry de la key sea el siguiente.
+            if(temp.next != null && temp.prev == null){
+                entries[hash] = temp.next;
+            } else if (temp.next == null && temp.prev == null) {
+                entries[hash] = null;
+            } else{
+                if(temp.next != null) {
+                    temp.next.prev = temp.prev;
+                }
+                temp.prev.next = temp.next;
+            }
+        }
+    }
+
+    private int getHash(String key) {
+        // piggy backing on java string
+        // hashcode implementation.
+        return key.hashCode() % INITIAL_SIZE;
+    }
+
+    private class HashEntry {
+        String key;
+        String value;
+
+        // Linked list of same hash entries.
+        HashEntry next;
+        HashEntry prev;
+
+        public HashEntry(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+            this.prev = null;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + key + ", " + value + "]";
+        }
+    }
+
+    @Override
+    public String toString() {
+        int bucket = 0;
+        //bucket++;
+        StringBuilder hashTableStr = new StringBuilder();
+        for (HashEntry entry : entries) {
+            if(entry == null) {
+                continue;
+            }
+            hashTableStr.append("\n bucket[")
+                    .append(bucket)
+                    .append("] = ")
+                    .append(entry.toString());
+            bucket++;
+            HashEntry temp = entry.next;
+            while(temp != null) {
+                hashTableStr.append(" -> ");
+                hashTableStr.append(temp.toString());
+                temp = temp.next;
+            }
+        }
+        return hashTableStr.toString();
+    }
+
+    public ArrayList<String> getCollisionsForKey(String key) {
+        return getCollisionsForKey(key, 1);
+    }
+
+    public ArrayList<String> getCollisionsForKey(String key, int quantity){
+        /*
+          Main idea:
+          alphabet = {0, 1, 2}
+
+          Step 1: "000"
+          Step 2: "001"
+          Step 3: "002"
+          Step 4: "010"
+          Step 5: "011"
+           ...
+          Step N: "222"
+
+          All those keys will be hashed and checking if collides with the given one.
+        * */
+
+        final char[] alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        ArrayList<Integer> newKey = new ArrayList();
+        ArrayList<String> foundKeys = new ArrayList();
+
+        newKey.add(0);
+        int collision = getHash(key);
+        int current = newKey.size() -1;
+
+        while (foundKeys.size() < quantity){
+            //building current key
+            String currentKey = "";
+            for(int i = 0; i < newKey.size(); i++)
+                currentKey += alphabet[newKey.get(i)];
+
+            if(!currentKey.equals(key) && getHash(currentKey) == collision)
+                foundKeys.add(currentKey);
+
+            //increasing the current alphabet key
+            newKey.set(current, newKey.get(current)+1);
+
+            //overflow over the alphabet on current!
+            if(newKey.get(current) == alphabet.length){
+                int previous = current;
+                do{
+                    //increasing the previous to current alphabet key
+                    previous--;
+                    if(previous >= 0)  newKey.set(previous, newKey.get(previous) + 1);
+                }
+                while (previous >= 0 && newKey.get(previous) == alphabet.length);
+
+                //cleaning
+                for(int i = previous + 1; i < newKey.size(); i++)
+                    newKey.set(i, 0);
+
+                //increasing size on underflow over the key size
+                if(previous < 0) newKey.add(0);
+
+                current = newKey.size() -1;
+            }
+        }
+
+        return  foundKeys;
+    }
+
+    private static void log(String msg) {
+        System.out.println(msg);
+    }
+}
